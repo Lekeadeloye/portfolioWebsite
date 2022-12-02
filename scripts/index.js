84489489ede96456f454ab635f80c3a6695d8e66
Lekeadeloye <lekeadeloye@yahoo.com>
2022-12-02 13:54:17 +0000

    Add second method to activate dark mode (using .toggle)

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,31 @@
+// let darkMode = document.getElementById("light-dark");
+
+// function toggleDarkMode() {
+//   let body = document.querySelector("body");
+//   let header = document.querySelector("header");
+//   let footer = document.querySelector("footer");
+
+//   body.style.backgroundColor = "black";
+//   body.style.color = "beige";
+//   header.style.backgroundColor = "white";
+//   header.style.color = "black";
+//   footer.style.backgroundColor = "white";
+//   footer.style.color = "white";
+// }
+
+// darkMode.addEventListener("click", toggleDarkMode);
+
+
+
 let darkMode = document.getElementById("light-dark");
 
-function toggleDarkMode() {
+darkMode.addEventListener("click", () => {
+
   let body = document.querySelector("body");
   let header = document.querySelector("header");
   let footer = document.querySelector("footer");
 
-  body.style.backgroundColor = "black";
-  body.style.color = "beige";
-  header.style.backgroundColor = "white";
-  header.style.color = "black";
-  footer.style.backgroundColor = "white";
-  footer.style.color = "white";
-}
-
-darkMode.addEventListener("click", toggleDarkMode);
+  body.classList.toggle("body2");
+  header.classList.toggle("header2");
+  footer.classList.toggle("footer2");
+});
