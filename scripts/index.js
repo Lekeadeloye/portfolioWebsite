60de7cc025e1274e0b525bfee552db865d089e91
Lekeadeloye <lekeadeloye@yahoo.com>
2023-01-27 12:17:35 +0000

    final version for hamburger menu in mobile view

--- scripts/index.js
+++ scripts/index.js
@@ -26,7 +26,12 @@ const navSlide = () => {
 
     // Animate Links
     navLinks.forEach((link, index) => {
-      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+      }
+      
     });
     
     // Burger Animation
